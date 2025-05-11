bc6a6d65e85be2bbca5cb0e8df1468ea1d563b44
Guidy ange isaac <packer02andro69@gmail.com>
2025-05-11 09:57:23 +0000

    Ajout d'une démonstration de gestion des contacts à la classe Main
    Cette mise à jour introduit un exemple d'utilisation dans la classe Main pour démontrer la création de contacts, leur ajout au ContactManager, et la recherche par nom. Cela aide à illustrer efficacement les fonctionnalités de la classe ContactManager.
    
    Signed-off-by: Guidy ange isaac <packer02andro69@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
+/**
+ * La classe Main sert de point d'entrée de l'application.
+ * Elle démontre les fonctionnalités de gestion des contacts utilisant la classe ContactManager.
+ * Les contacts sont créés, ajoutés au gestionnaire, et recherchés par nom.
+ */
 public class Main {
     public static void main(String[] args) {
+        Contact contact = new Contact("cheick", "0502162069");
+        Contact contact1 = new Contact("abou", "0502162069");
+        Contact contact2 = new Contact("guidy", "0502162069");
+        Contact contact3 = new Contact("trazie bi", "0502162069");
+        ContactManager contactManager = new ContactManager();
+        contactManager.addFriend(contact);
+        contactManager.addFriend(contact1);
+        contactManager.addFriend(contact2);
+        contactManager.addFriend(contact3);
+        System.out.println(contactManager.searchContact("cheick").name);
 
     }
 }
