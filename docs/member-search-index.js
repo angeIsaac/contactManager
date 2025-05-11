b67e8d3184c01d4665a7707639eba21eca9f4709
Guidy ange isaac <packer02andro69@gmail.com>
2025-05-11 09:57:54 +0000

    Add jQuery UI v1.13.2 minified script to docs
    This commit introduces the minified version of jQuery UI v1.13.2 to the documentation folder. It includes essential widget and utility scripts like autocomplete, menu, and more, licensed under MIT. This enhances functionality and ensures compatibility for the project.
    
    Signed-off-by: Guidy ange isaac <packer02andro69@gmail.com>

---
+++ docs/member-search-index.js
@@ -0,0 +1,20 @@
+memberSearchIndex = [{"p": "<Unnamed>", "c": "ContactManager", "l": "addFriend(Contact)"}, {
+    "p": "<Unnamed>",
+    "c": "ContactManager",
+    "l": "friendCount"
+}, {"p": "<Unnamed>", "c": "Main", "l": "Main()", "u": "%3Cinit%3E()"}, {
+    "p": "<Unnamed>",
+    "c": "Main",
+    "l": "main(String[])",
+    "u": "main(java.lang.String[])"
+}, {"p": "<Unnamed>", "c": "ContactManager", "l": "myFriends"}, {
+    "p": "<Unnamed>",
+    "c": "Contact",
+    "l": "name"
+}, {"p": "<Unnamed>", "c": "Contact", "l": "phone"}, {
+    "p": "<Unnamed>",
+    "c": "ContactManager",
+    "l": "searchContact(String)",
+    "u": "searchContact(java.lang.String)"
+}];
+updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"l": "All Classes and Interfaces", "u": "allclasses-index.html"}, {
+    "p": "<Unnamed>",
+    "l": "Contact"
+}, {"p": "<Unnamed>", "l": "ContactManager"}, {"p": "<Unnamed>", "l": "Main"}];
+updateSearchResults();
